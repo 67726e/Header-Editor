458fd0a33e1b3fbc13590bf4804d693a1ee6493f
Glenn Nelson <glenn@hexcoder.us>
2013-02-12 19:57:27 -0500

    Update package script to build ZIP

--- package.sh
+++ package.sh
@@ -3,13 +3,26 @@
 # Copies all project files into a build directory so non-extension files are not included in the package
 
 BUILD="./build"
+DISTRIBUTION="Header-Editor"
+ZIP="${DISTRIBUTION}.zip"
 
 if [ -e ${BUILD} ]
 then
 	rm -r ${BUILD}
 fi
 
+if [ -e /tmp/${DISTRIBUTION} ]
+then
+	rm -r /tmp/${DISTRIBUTION}
+fi
+
+if [ -e /tmp/${ZIP} ]
+then
+	rm /tmp/${ZIP}
+fi
+
 mkdir ${BUILD}
+mkdir /tmp/${DISTRIBUTION}
 
 cp ./manifest.json ${BUILD}
 cp -a ./_locales ${BUILD}
@@ -20,3 +33,8 @@ cp -a ./views ${BUILD}
 
 # This file is a stub and only used for auto-completion by IntelliJ
 rm ${BUILD}/libs/chrome_extensions.js
+
+# Copy the ready-to-package files to `/tmp` & ZIP them
+cp -a ${BUILD}/* /tmp/${DISTRIBUTION}
+cd /tmp
+zip -r ${ZIP} ${DISTRIBUTION}/*

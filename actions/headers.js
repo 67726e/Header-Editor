8bc9b1a45db051342823ecfbcd86193dfac41208
Glenn Nelson <glenn@hexcoder.us>
2013-07-27 15:54:04 -0400

    Added code to insert response headers

--- actions/headers.js
+++ actions/headers.js
@@ -4,39 +4,76 @@
 (function() {
 	"use strict";
 
-	function getChromeHeaders() {
-		var headers = window.getRequestHeaders();
+	// Insert request headers
+	(function() {
+		function getChromeHeaders() {
+			var headers = window.getRequestHeaders();
 
-		var chromeHeaders = [];
+			var chromeHeaders = [];
 
-		for (var key in headers) {
-			if (headers.hasOwnProperty(key) && headers[key].active === true) {
-				chromeHeaders.push({
-					name: headers[key].header,
-					value: headers[key].value
-				});
+			for (var key in headers) {
+				if (headers.hasOwnProperty(key) && headers[key].active === true) {
+					chromeHeaders.push({
+						name: headers[key].header,
+						value: headers[key].value
+					});
+				}
 			}
+
+			return chromeHeaders;
 		}
 
-		return chromeHeaders;
-	}
+		var chromeHeaders = getChromeHeaders();
+		var lastModified = window.getRequestLastModified();
 
-	var chromeHeaders = getChromeHeaders();
-	var lastModified = window.getRequestLastModified();
+		var modifyHeaders = function(details) {
+			if (lastModified !== window.getRequestLastModified()) {
+				chromeHeaders = getChromeHeaders();
+			}
 
+			details.requestHeaders = details.requestHeaders.concat(chromeHeaders);
+			return {requestHeaders: details.requestHeaders};
+		};
 
-	var modifyHeaders = function(details) {
-		if (lastModified !== window.getRequestLastModified()) {
-			chromeHeaders = getChromeHeaders();
+		var filter = {urls: ["http://*/*", "https://*/*"]};
+		var extra = ["requestHeaders", "blocking"];
+
+		chrome.webRequest.onBeforeSendHeaders.addListener(modifyHeaders, filter, extra);
+	})();
+
+	// Insert response headers
+	(function() {
+		function getResponseHeaders() {
+			var headers = window.getResponseHeaders();
+			var chromeHeaders = [];
+
+			for (var key in headers) {
+				if (headers.hasOwnProperty(key) && headers[key].active === true) {
+					chromeHeaders.push({
+						name: headers[key].header,
+						value: headers[key].value
+					});
+				}
+			}
+
+			return chromeHeaders;
 		}
 
-		details.requestHeaders = details.requestHeaders.concat(chromeHeaders);
-		return {requestHeaders: details.requestHeaders};
-	};
+		var responseHeaders = getResponseHeaders();
+		var lastModified = window.getResponseLastModified();
 
-	var filter = {urls: ["http://*/*", "https://*/*"]};
-	var extra = ["requestHeaders", "blocking"];
+		var modifyResponse = function(details) {
+			if (lastModified !== window.getRequestLastModified()) {
+				responseHeaders = getResponseHeaders();
+			}
 
-	chrome.webRequest.onBeforeSendHeaders.addListener(modifyHeaders, filter, extra);
+			details.responseHeaders = details.responseHeaders.concat(responseHeaders);
+			return {responseHeaders: details.responseHeaders};
+		};
 
+		var filter = {urls: ["http://*/*", "https://*/*"]};
+		var extra = ["responseHeaders", "blocking"];
+
+		chrome.webRequest.onHeadersReceived.addListener(modifyResponse, filter, extra);
+	})();
 })();
